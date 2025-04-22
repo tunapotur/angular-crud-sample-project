718c11bf7224ac8925c0ecf5d899338ad3a82bc2
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-04-22 22:16:14 +0300

    delete code block

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -55,30 +55,3 @@ router.post("/login", async (req, res) => {
 });
 
 module.exports = router;
-/**
- * 
-import { NextResponse } from "next/server";
-import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/User";
-import bcrypt from "bcryptjs";
-
-export async function POST(req) {
-  try {
-    const { name, email, password } = await req.json();
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await connectMongoDB();
-    await User.create({ name, email, password: hashedPassword });
-
-    return NextResponse.json({ message: "User registered." }, { status: 201 });
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: `An error occurred while registering the user. ${error.message}`,
-      },
-      { status: 500 },
-    );
-  }
-}
-
- */
